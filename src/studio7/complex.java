71220ca86221c407f781c2f85cadbd215327e8d6
Foliage Song <p.song@wustl.edu>
2025-11-05 14:53:57 -0600

    Update twoComplex.java

--- src/studio7/complex.java
+++ src/studio7/complex.java
@@ -1,3 +1,21 @@
-public class complex {
-    
+public class twoComplex {
+    public class twoComplex {
+    private int real1;
+    private int imag1;
+    private int real2;
+    private int imag2;
+    public twoComplex(int real1, int imag1, int real2, int imag2){
+        this.real1 = real1;
+        this.imag1 = imag1;
+        this.real2 = real2;
+        this.imag2 = imag2;
+    }
+    public String addition(){
+        return real1 + real2 + "+" + imag1 + imag2 + "i";
+    }
+    public String multiplication(){
+        return (real1 * real2 - imag1 * imag2) + "+" + (real1 * imag2 + imag1 * real2) + "i";
+    }
+}
+
 }
